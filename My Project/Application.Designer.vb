51680b140431cef6753aaff4b5ff6b7ca0d4baa2
Russell Radant <rrradant@live.com>
2021-07-26 09:24:01 -0400

    OK

--- My Project/Application.Designer.vb
+++ My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.VacuDraw_ModBus.MainForm
+            Me.MainForm = Global.VacuDraw_DAQ.MainForm
         End Sub
     End Class
 End Namespace
